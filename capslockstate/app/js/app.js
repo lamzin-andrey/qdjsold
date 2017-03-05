71504ebe5be1f6048a947fba4d23615a89d109f2
Andrey Lamzin <lamzin.an@gmail.com>
2017-03-05 18:34:25 +0300

    Воркер для более четкой работы индикатора capslock

--- capslockstate/app/js/app.js
+++ capslockstate/app/js/app.js
@@ -3,12 +3,12 @@ Qt.hide();
 var Exec = PHP,
 	executeProc = false;
 
-setInterval(function(){
+/*setInterval(function(){
 	if (!executeProc) {
 		executeProc = true;
 		PHP.exec(Qt.appDir() + '/capslockstate', 'onFinish' );
 	}
-}, 500);
+}, 1000);*/
 
 function onFinish(stdin, stdout) {
 	executeProc = false;
@@ -16,5 +16,15 @@ function onFinish(stdin, stdout) {
 		Qt.show();
 	} else {
 		Qt.hide();
+	}
+}
+
+var w = new Worker('./js/wkr.js');
+w.addEventListener('message', onMessage);
+function onMessage(e) {
+	if (!executeProc) {
+		executeProc = true;
+		PHP.exec(Qt.appDir() + '/capslockstate', 'onFinish' );
+		//document.body.innerHTML = e.data;
 	}
 }

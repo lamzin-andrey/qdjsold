363778ec7dd5731798cdeaf0f9240b0655d75585
Andrey Lamzin <lamzin.an@gmail.com>
2018-07-24 13:07:07 +0300

    jaqMp3Cut Добавлено событие окончания процесса

--- jaqMp3cut/app/js/app.js
+++ jaqMp3cut/app/js/app.js
@@ -1,5 +1,6 @@
 window.onload = init;
 function init(){
+	window.procId = 0;
 	e('browse').onclick = jmp3cutOnBrowse;
 	e('convert').onclick = jmp3cutOnConvertClick;
 }
@@ -14,21 +15,54 @@ function jmp3cutOnConvertClick() {
 		//cmd = 'cd ' + jmp3cutGetDir() + ';\navconv -i ' + jmp3cutGetName() + ' -ss ' + e('start').value + ' -t ' + e('duration').value + ' ' +
 			jmp3cutGetOutfile();
 		
-		PHP.exec(name, 'jmp3cutOnFinish', 'jmp3cutOnStd', 'jmp3cutOnErr');
+		procId = PHP.exec(name, 'jmp3cutOnFinish', 'jmp3cutOnStd', 'jmp3cutOnErr');
 		//TODO следить за изменением размера файла, если перестал увеличиваться, значит финиш
+		setInterval(jmp3Observe, 200);
 	} else {
 		alert('Вам надо выбрать mp3 файл');
 	}
 }
-function jmp3cutOnStd(std) {
+function jmp3Observe() {
+	if (window.observeProcIsRun) {
+		return;
+	}
+	observeProcIsRun = 1;
+	window.sysId = PHP.getSysId(procId);
+	PHP.exec('ps -Ac', 'jmp3cutOnObserveFinish', 'jmp3cutOnObserveStd', 'jmp3cutOnObserveErr');
+}
+function jmp3cutOnObserveStd(std) {
 	//alert('Live std = ' + std);
 }
-function jmp3cutOnErr(err) {
+function jmp3cutOnObserveErr(err) {
 	//alert('Live err = ' + err);
 }
+function jmp3cutOnObserveFinish(std, err) {
+	var a = std.split('\n'), i, b, n, found;
+	for (i = 0; i < a.length; i++) {
+		b = a[i].split(' ');
+		n = parseInt(b[0].trim(), 10);
+		if (!isNaN(n)) {
+			if (sysId == n) {
+				found = true;
+				break;
+			}
+		}
+	}
+	if (!found) {
+		jmp3cutOnComplete();
+	}
+	window.observeProcIsRun = 0;
+	
+}
+
+function jmp3cutOnStd(std) {}
+function jmp3cutOnErr(err) {}
 function jmp3cutOnFinish(std, err) {
-	alert('Fin std = ' + std);
-	alert('Fin err = ' + err);
+	alert('Fin  std = ' + std);
+	alert('Fin  err = ' + err);
+}
+function jmp3cutOnComplete() {
+	alert('done');
 }
 //TODO сохранять последнюю директорию
 function jmp3cutOnBrowse() {
--- jaqMp3cut/app/sh.sh
+++ jaqMp3cut/app/sh.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd /home/andrey/hdata/programs/my/qdjs/jaqMp3cut/app;
-avconv -i 3.mp3 -ss 00:00:00 -t 00:01:02 3-out.mp3
+avconv -i 0.mp3 -ss 00:00:00 -t 00:00:05 0-out.mp3

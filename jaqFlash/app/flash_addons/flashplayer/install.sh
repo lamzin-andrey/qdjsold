01fe3fbe8c8bac0a2884de62e05528c49238acf6
Andrey Lamzin <lamzin.an@gmail.com>
2019-12-24 15:34:36 +0300

    Движение в сторону установки flashplugin - а

---
+++ jaqFlash/app/flash_addons/flashplayer/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo mkdir /var/cache/flashplugin-installer
+sudo mkdir /usr/lib/mozilla
+sudo mkdir /usr/lib/mozilla/plugins
+sudo mkdir /usr/lib/flashplugin-installer
+sudo /media/xubuntu/land/packages-new/flashplayer/install_plugin /media/xubuntu/land/packages-new/flashplayer/adobe-flashplugin_20170912.1.orig.tar.gz

efcab0b096cecda6ca02240fa459975d9f64c14f
andrey <andrey@andrey-spare>
2020-04-02 23:07:54 +0400

    good plugin

--- jaqFlash/app/rununpack.sh
+++ jaqFlash/app/rununpack.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-dd if="/home/andrey/Werewars-Fight.swf" of="/home/andrey/hdata/programs/my/qdjs/jaqFlash/app/0.swf.gz" skip=8 ibs=1
+dd if="/media/andrey/KINGSTON/games/formularacer2012.swf" of="/home/andrey/hdata/programs/my/qdjs/jaqFlash/app/0.swf.gz" skip=8 ibs=1
 php /home/andrey/hdata/programs/my/qdjs/jaqFlash/app/testungz.php
